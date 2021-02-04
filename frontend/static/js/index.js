6b490341973a18b5c546a94e766416998617aafd
Jesiel Palacios <jopalacioss@unac.edu.co>
2021-02-04 12:19:29 -0500

    Agrega estilos y rutas de la app

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,7 @@
+import Dashboard from './views/Dashboard.js';
+import Posts from './views/Posts.js';
+import Settings from './views/Settings.js';
+
 const navigateTo = url => { // Para navegar entre las rutas desde el navegador
     history.pushState(null, null, url); // Coloca las rutas en el historial.
     router();
@@ -5,9 +9,12 @@ const navigateTo = url => { // Para navegar entre las rutas desde el navegador
 
 const router = async () => {
     const routes = [ // Arreglo de rutas
-        { path: "/", view: () => console.log('Viewing Dashboard') },
-        { path: "/posts", view: () => console.log('Viewing Posts') },
-        { path: "/settings", view: () => console.log('Viewing Settings') }
+        // { path: "/", view: () => console.log('Viewing Dashboard') },
+        // { path: "/posts", view: () => console.log('Viewing Posts') },
+        // { path: "/settings", view: () => console.log('Viewing Settings') }
+        { path: "/", view: Dashboard },
+        { path: "/posts", view: Posts },
+        { path: "/settings", view: Settings }
     ];
 
     // Test each route for potential match
@@ -28,15 +35,20 @@ const router = async () => {
             isMatch: true    
         };
     }
+
+    const view = new match.route.view();
+    
     // console.log(match)
-    console.log(match.route.view());
+    // console.log(match.route.view());
     // Revisar por la consola del navegador: location.pathname
+
+    document.querySelector('#app').innerHTML = await view.getHtml();
 };
 
 window.addEventListener('popstate', router); // permite que se pueda navegar por medio de los botones de navegación del navegador
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.addEventListener('click', () => {
+    document.body.addEventListener('click', e => {
         if (e.target.matches('[data-link]')) {
             e.preventDefault();
             navigateTo(e.target.href);
